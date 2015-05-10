ed5ec226ac4755d0531f6be82bcf2e95affe55f6
Joe Mercer <mynameisjoemercer@gmail.com>
2015-05-10 13:24:49 -0400

    updated jshintrc

--- app/scripts/services/db.js
+++ app/scripts/services/db.js
@@ -9,21 +9,8 @@
  */
 angular.module('resumablyApp')
   .service('db', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
 
-    this._db = [{
-    	tags: ['experience', 'dev'],
-    	data: {
-    		company: 'Noom',
-    		position: 'Software Engineer'
-    	}
-    },{
-    	tags: ['experience', 'pm'],
-    	data: {
-    		company: 'Microsoft',
-    		position: 'Project Manager'
-    	}
-    }];
+    this._db = [{"tags":["experience","dev"],"data":{"company":"Noom","position":"Software Engineer"}},{"tags":["experience","pm"],"data":{"company":"Microsoft","position":"Project Manager"}}];
 
     this.push = function() {
 
